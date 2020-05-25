0088a9190ae68029e33dfe78edac92381cfaa9a6
daphino <111201911748@mhs.dinus.ac.id>
2020-05-25 21:55:26 +0700

    Install laravel/ui

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Certificate Generator
 ``On Progress``
+[![vue2](https://img.shields.io/badge/vue-2.x-brightgreen.svg)](https://vuejs.org/)
--- public/js/app.js
+++ public/js/app.js
@@ -19293,27 +19293,8 @@ __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 /***/ (function(module, exports, __webpack_require__) {
 
 window._ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-// import Echo from 'laravel-echo';
-// window.Pusher = require('pusher-js');
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
 
 /***/ }),
 
@@ -19335,8 +19316,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\11120\Documents\certificate\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\11120\Documents\certificate\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/sedaph/Desktop/certificate/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/sedaph/Desktop/certificate/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
